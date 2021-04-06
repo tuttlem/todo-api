c8f3e5b8d07594b4c31a10850ea79dd813b3f9a7
Michael Tuttle <tuttlem@gmail.com>
2021-04-06 21:08:05 +1000

    Base server running

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,26 @@
 module Lib
-    ( someFunc
+    ( main
     ) where
 
 import ClassyPrelude
+import Crypto.Random.Types (MonadRandom, getRandomBytes)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Platform.HTTP as HTTP
+
+main :: IO ()
+main = do
+    -- acquire resources
+
+    -- start the application
+    let runner app = flip runReaderT () $ unAppT app
+    HTTP.main runner
+
+type Env = ()
+
+newtype AppT a = AppT
+    { unAppT :: ReaderT Env IO a
+    } deriving ( Applicative, Functor, Monad
+               , MonadIO, MonadReader Env)
+
+instance MonadRandom AppT where
+    getRandomBytes = liftIO . getRandomBytes
